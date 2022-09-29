3f01570850b1441d14ff63eaafcc61183a267357
Taher027 <abutaherh27@gmail.com>
2022-09-29 22:13:32 +0600

    added blog

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -13,6 +13,9 @@ const Blog = () => {
             <p>In props, The Data is passed from one component to another. It is Immutable (cannot be modified). Props can be used with state and functional components.</p>
             <p>In state, The Data is passed within the component only. State can be used only with the state components/class component (Before 16.0). State is both read and write.</p>
 
+            <h2>Without load data what can we do with useState?</h2>
+            <p>The useState() is a Hook that allows you to have state variables in functional components . so basically useState is the ability to encapsulate local state in a functional component. React has two types of components, one is class components which are ES6 classes that extend from React and the other is functional components. Class components a Component and can have state and lifecycle methods: class Message extends React. The  useState hook is a special function that takes the initial state as an argument and returns an array of two entries.  UseState encapsulate only singular value from the state, for multiple state need to have useState calls.</p>
+
         </div>
     );
 };
